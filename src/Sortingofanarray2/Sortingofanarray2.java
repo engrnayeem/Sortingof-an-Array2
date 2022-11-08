3e20ed7810e8ee965ea754d63689233933a67cb7
Mohammad Jannatul Nayeem <90479153+engrnayeem@users.noreply.github.com>
2022-11-08 20:01:33 +0600

    Update Sortingofanarray2.java

--- src/Sortingofanarray2/Sortingofanarray2.java
+++ src/Sortingofanarray2/Sortingofanarray2.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 public class Sortingofanarray2 {
 
     public static void main(String[] args) {
+        //descending order
         //variable declaration area
         int i;
         int[] x = {7, 8, 6, 4, 2, 1, 3, 5};
